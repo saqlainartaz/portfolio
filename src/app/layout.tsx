28cddb05bdeb2a94a411e5325e5028fb366c62cc
unknown <saqlainartaz@gmail.com>
2025-03-27 16:44:37 +0100

    fixing vercel anayltics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -95,6 +95,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
         code.variable,
       )}
     >
+    <Analytics />
     {/* Google Analytics */}
     <Script
       strategy="afterInteractive"
